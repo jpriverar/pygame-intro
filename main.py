5f90dc186f3e7f50bdb9de706db0d1611aed573a
Juan Pablo Rivera <rivera.ruiz.juanp@gmail.com>
2024-11-30 16:41:13 -0600

    handle exit in single if statement

--- main.py
+++ main.py
@@ -53,14 +53,9 @@ pygame.time.set_timer(obstacle_timer, 1000)
 
 while True:
     for event in pygame.event.get():
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE):
             pygame.quit()
             sys.exit()
-
-        elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                pygame.quit()
-                sys.exit()
 
         if game_over: 
             if event.type == pygame.KEYDOWN:
